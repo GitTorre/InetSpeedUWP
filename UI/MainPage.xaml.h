7af19ce9617007fddcb2b6ebaf3d13eafe6d488e
Gittorre <torre_charles@hotmail.com>
2015-06-16 17:30:28 -0700

    Fixed for latest TH build...

---
+++ UI/MainPage.xaml.h
@@ -0,0 +1,27 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+#include "ppl.h"
+
+using namespace InetSpeedUAP;
+using namespace Concurrency;
+
+namespace UI
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+	private:
+		void SpeedButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		ConnectionSpeed __speed;
+	};
+}
